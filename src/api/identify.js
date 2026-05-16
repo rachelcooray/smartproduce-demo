c5a03f8306f106f8f6689aef72f5cc98cd4d7bdd
rachelcooray <rachelcooray@gmail.com>
2026-05-16 22:42:24 +0530

    Set USE_ANTHROPIC_FALLBACK=false

--- src/api/identify.js
+++ src/api/identify.js
@@ -1,6 +1,6 @@
 import { runOnnx } from './onnxInfer'
 
-const USE_ANTHROPIC_FALLBACK = true
+const USE_ANTHROPIC_FALLBACK = false
 
 // Returns { result, model } where model is 'onnx' or 'claude'
 export async function identifyProduce(base64ImageData, onnxSession) {
